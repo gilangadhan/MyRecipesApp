5e6c809083e0bc5768dd24af73e30e8b7edba2ff
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2021-10-06 13:00:36 +0700

    Add Adapter and Data Model

--- app/src/main/java/com/dicoding/picodiploma/myrecipesapp/ui/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/myrecipesapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dicoding.picodiploma.myrecipesapp
+package com.dicoding.picodiploma.myrecipesapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dicoding.picodiploma.myrecipesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
